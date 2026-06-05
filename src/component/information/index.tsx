9206c3a8839ca7edd7539b9f19e60c836783a8fa
hunjunee <hunjunee@naver.com>
2026-06-05 17:07:23 +0900

    260605 5

--- src/component/information/index.tsx
+++ src/component/information/index.tsx
@@ -18,7 +18,7 @@ export const Information1 = () => {
         <div className="content">
           식사시간: 00시 00분 ~ 00시 00분
           <br />
-          장소: 2층 연회장 (홀은 1층입니다.)
+          연회장소: 2층 연회장
         </div>
       </div>
     </>
