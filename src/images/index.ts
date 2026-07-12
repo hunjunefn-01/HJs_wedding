21a7e8061cc4b0ccb435dcf182b7c9f7d99a0a53
hunjunee <hunjunee@naver.com>
2026-07-12 14:27:48 +0900

    add images remaked from jinseok

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,34 +1,57 @@
-import coverImage from "./원본 260516_헌준웨딩0177.jpg"
-import image02 from "./원본 260516_헌준웨딩0196.jpg"
-import image03 from "./원본 260516_헌준웨딩0251.jpg"
-import image04 from "./원본 260516_헌준웨딩0500.jpg"
-import image05 from "./원본 260516_헌준웨딩0620.jpg"
-import image06 from "./원본 260516_헌준웨딩0708.jpg"
-import image07 from "./원본 260516_헌준웨딩0777.jpg"
-import image08 from "./원본 260516_헌준웨딩0840.jpg"
-import image09 from "./원본 260516_헌준웨딩0859.jpg"
-import image10 from "./원본 260516_헌준웨딩0962.jpg"
-import image11 from "./원본 260516_헌준웨딩0997.jpg"
-import image12 from "./원본 260516_헌준웨딩1076.jpg"
-import image13 from "./원본 260516_헌준웨딩1136.jpg"
-import image14 from "./원본 260516_헌준웨딩1322.jpg"
-import image15 from "./원본 260516_헌준웨딩1410.jpg"
-import image16 from "./원본 260516_헌준웨딩1664.jpg"
-import image17 from "./원본 260516_헌준웨딩1910.jpg"
-import image18 from "./원본 260516_헌준웨딩1931.jpg"
-import image19 from "./원본 260516_헌준웨딩1999.jpg"
-import image20 from "./원본 260516_헌준웨딩2017.jpg"
-import image21 from "./원본 260516_헌준웨딩2196.jpg"
-import image22 from "./원본 260516_헌준웨딩2248.jpg"
-import image23 from "./원본 260516_헌준웨딩2288.jpg"
-import image24 from "./원본 260516_헌준웨딩2306.jpg"
-import image25 from "./원본 260516_헌준웨딩2523.jpg"
-import image26 from "./원본 260516_헌준웨딩2713.jpg"
-import image27 from "./원본 260516_헌준웨딩2736.jpg"
-import image28 from "./원본 260516_헌준웨딩2808.jpg"
-import image29 from "./원본 260516_헌준웨딩2832.jpg"
-import image30 from "./원본 260516_헌준웨딩2873.jpg"
-import image31 from "./원본 260516_헌준웨딩3003.jpg"
+import coverImage from "./1.jpg"
+import image2 from "./2.jpg"
+import image3 from "./3.jpg"
+import image4 from "./4.jpg"
+import image5 from "./5.jpg"
+import image6 from "./6.jpg"
+import image7 from "./7.jpg"
+import image8 from "./8.jpg"
+import image9 from "./9.jpg"
+import image10 from "./10.jpg"
+import image11 from "./11.jpg"
+import image12 from "./12.jpg"
+import image13 from "./13.jpg"
+import image14 from "./14.jpg"
+import image15 from "./15.jpg"
+import image16 from "./16.jpg"
+import image17 from "./17.jpg"
+import image18 from "./18.jpg"
+import image19 from "./19.jpg"
+import image20 from "./20.jpg"
+import image21 from "./21.jpg"
+import image22 from "./22.jpg"
+import image23 from "./23.jpg"
+import image24 from "./24.jpg"
+import image25 from "./25.jpg"
+import image26 from "./26.jpg"
+import image27 from "./27.jpg"
+import image28 from "./28.jpg"
+import image29 from "./29.jpg"
+import image30 from "./30.jpg"
+import image31 from "./31.jpg"
+import image32 from "./32.jpg"
+import image321 from "./32-1.jpg"
+import image322 from "./32-2.jpg"
+import image33 from "./33.jpg"
+import image34 from "./34.jpg"
+import image35 from "./35.jpg"
+import image36 from "./36.jpg"
+import image37 from "./37.jpg"
+import image38 from "./38.jpg"
+import image39 from "./39.jpg"
+import image40 from "./40.jpg"
+import image41 from "./41.jpg"
+import image42 from "./42.jpg"
+import image43 from "./43.jpg"
+import image44 from "./44.jpg"
+import image45 from "./45.jpg"
+import image46 from "./46.jpg"
+import image47 from "./47.jpg"
+import image48 from "./48.jpg"
+import image49 from "./49.jpg"
+import image50 from "./50.jpg"
+import image51 from "./51.jpg"
+import image52 from "./52.jpg"
 
 /**
  * 메인 커버 이미지
@@ -39,34 +62,58 @@ export const COVER_IMAGE = coverImage
  * 갤러리에 표시될 이미지 목록
  */
 export const GALLERY_IMAGES = [
-  image02,
-  image03,
-  image04,
-  image05,
-  image06,
-  image07,
-  image08,
-  image09,
-  image10,
-  image11,
-  image12,
-  image13,
-  image14,
-  image15,
-  image16,
-  image17,
-  image18,
-  image19,
-  image20,
-  image21,
-  image22,
-  image23,
-  image24,
-  image25,
-  image26,
-  image27,
-  image28,
-  image29,
-  image30,
-  image31,
+coverImage,
+image2,
+image3,
+image4,
+image5,
+image6,
+image7,
+image8,
+image9,
+image10,
+image11,
+image12,
+image13,
+image14,
+image15,
+image16,
+image17,
+image18,
+image19,
+image20,
+image21,
+image22,
+image23,
+image24,
+image25,
+image26,
+image27,
+image28,
+image29,
+image30,
+image31,
+image32,
+image321,
+image322,
+image33,
+image34,
+image35,
+image36,
+image37,
+image38,
+image39,
+image40,
+image41,
+image42,
+image43,
+image44,
+image45,
+image46,
+image47,
+image48,
+image49,
+image50,
+image51,
+image52,
 ]
---
+++ src/images_backup/index.ts
@@ -0,0 +1,72 @@
+import coverImage from "./원본 260516_헌준웨딩0177.jpg"
+import image02 from "./원본 260516_헌준웨딩0196.jpg"
+import image03 from "./원본 260516_헌준웨딩0251.jpg"
+import image04 from "./원본 260516_헌준웨딩0500.jpg"
+import image05 from "./원본 260516_헌준웨딩0620.jpg"
+import image06 from "./원본 260516_헌준웨딩0708.jpg"
+import image07 from "./원본 260516_헌준웨딩0777.jpg"
+import image08 from "./원본 260516_헌준웨딩0840.jpg"
+import image09 from "./원본 260516_헌준웨딩0859.jpg"
+import image10 from "./원본 260516_헌준웨딩0962.jpg"
+import image11 from "./원본 260516_헌준웨딩0997.jpg"
+import image12 from "./원본 260516_헌준웨딩1076.jpg"
+import image13 from "./원본 260516_헌준웨딩1136.jpg"
+import image14 from "./원본 260516_헌준웨딩1322.jpg"
+import image15 from "./원본 260516_헌준웨딩1410.jpg"
+import image16 from "./원본 260516_헌준웨딩1664.jpg"
+import image17 from "./원본 260516_헌준웨딩1910.jpg"
+import image18 from "./원본 260516_헌준웨딩1931.jpg"
+import image19 from "./원본 260516_헌준웨딩1999.jpg"
+import image20 from "./원본 260516_헌준웨딩2017.jpg"
+import image21 from "./원본 260516_헌준웨딩2196.jpg"
+import image22 from "./원본 260516_헌준웨딩2248.jpg"
+import image23 from "./원본 260516_헌준웨딩2288.jpg"
+import image24 from "./원본 260516_헌준웨딩2306.jpg"
+import image25 from "./원본 260516_헌준웨딩2523.jpg"
+import image26 from "./원본 260516_헌준웨딩2713.jpg"
+import image27 from "./원본 260516_헌준웨딩2736.jpg"
+import image28 from "./원본 260516_헌준웨딩2808.jpg"
+import image29 from "./원본 260516_헌준웨딩2832.jpg"
+import image30 from "./원본 260516_헌준웨딩2873.jpg"
+import image31 from "./원본 260516_헌준웨딩3003.jpg"
+
+/**
+ * 메인 커버 이미지
+ */
+export const COVER_IMAGE = coverImage
+
+/**
+ * 갤러리에 표시될 이미지 목록
+ */
+export const GALLERY_IMAGES = [
+  image02,
+  image03,
+  image04,
+  image05,
+  image06,
+  image07,
+  image08,
+  image09,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20,
+  image21,
+  image22,
+  image23,
+  image24,
+  image25,
+  image26,
+  image27,
+  image28,
+  image29,
+  image30,
+  image31,
+]
