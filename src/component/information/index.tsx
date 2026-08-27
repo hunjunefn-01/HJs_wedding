750257661350012246334c3cb14ba10fd07d4c35
hunjunee <hunjunee@naver.com>
2026-08-28 01:23:18 +0900

    Revert "Remove meal info box, restyle gift-account section without card border"
    This reverts commit 06ae5ff4a5ffdfb46f1e6defdd821ff97dd557b1.

--- src/component/information/index.tsx
+++ src/component/information/index.tsx
@@ -5,6 +5,25 @@ import { LazyDiv } from "../lazyDiv"
 import { Modal } from "../modal"
 
 /**
+ * 식사 정보 안내 컴포넌트입니다.
+ */
+export const Information1 = () => {
+  return (
+    <>
+      <h2 className="english">Information</h2>
+      <div className="info-card">
+        <div className="label">식사 안내</div>
+        <div className="content">
+          식사시간: 00시 00분 ~ 00시 00분
+          <br />
+          연회장소: 2층 연회장
+        </div>
+      </div>
+    </>
+  )
+}
+
+/**
  * 축의금 계좌번호 안내 컴포넌트입니다.
  * 신랑측, 신부측 계좌번호를 모달로 보여줍니다.
  */
@@ -14,36 +33,38 @@ export const Information2 = () => {
 
   return (
     <>
-      <div className="heading">마음 전하기</div>
-      <div className="content">
-        참석이 어려우신 분들을 위해
-        <br />
-        마음 전하실 계좌를 안내드립니다.
-        <br />
-        넓은 마음으로 양해해 주시면 감사하겠습니다.
+      <div className="info-card">
+        <div className="label">마음 전하기</div>
+        <div className="content">
+          참석이 어려우신 분들을 위해
+          <br />
+          마음 전하실 계좌를 안내드립니다.
+          <br />
+          넓은 마음으로 양해해 주시면 감사하겠습니다.
+        </div>
+
+        <div className="break" />
+
+        <Button
+          style={{ width: "100%" }}
+          onClick={() => {
+            donationModalState[1](true)
+            setIsGroom(true)
+          }}
+        >
+          신랑측 계좌번호 보기
+        </Button>
+        <div className="break" />
+        <Button
+          style={{ width: "100%" }}
+          onClick={() => {
+            donationModalState[1](true)
+            setIsGroom(false)
+          }}
+        >
+          신부측 계좌번호 보기
+        </Button>
       </div>
-
-      <div className="break" />
-
-      <Button
-        style={{ width: "100%" }}
-        onClick={() => {
-          donationModalState[1](true)
-          setIsGroom(true)
-        }}
-      >
-        신랑측 계좌번호 보기
-      </Button>
-      <div className="break" />
-      <Button
-        style={{ width: "100%" }}
-        onClick={() => {
-          donationModalState[1](true)
-          setIsGroom(false)
-        }}
-      >
-        신부측 계좌번호 보기
-      </Button>
 
       {/* 계좌 정보 모달 */}
       <Modal
@@ -101,14 +122,14 @@ export const Information2 = () => {
 }
 
 /**
- * 정보 안내(축의금) 섹션을 표시하는 컴포넌트입니다.
+ * 정보 안내(식사, 축의금)를 통합하여 표시하는 컴포넌트입니다.
  *
  * @returns {JSX.Element} 정보 안내 섹션
  */
 export const Information = () => {
   return (
     <LazyDiv className="card information">
-      <h2 className="english">Information</h2>
+      <Information1 />
       <Information2 />
     </LazyDiv>
   )
